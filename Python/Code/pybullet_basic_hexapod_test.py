908d0b265ca8268b427f49cbc1a5241d99f3be97
salehrayan <138001028+salehrayan@users.noreply.github.com>
2024-06-02 19:50:00 +0330

    s

--- Python/Code/pybullet_basic_hexapod_test.py
+++ Python/Code/pybullet_basic_hexapod_test.py
@@ -2,7 +2,7 @@ import pybullet_data
 import pybullet as p
 from pybullet_utils import bullet_client
 import numpy as np
-import time
+from time import sleep
 from utils import *
 
 
@@ -10,16 +10,26 @@ from utils import *
 hexapod_urdf_path = r'C:\Users\ASUS\Desktop\Re-inforcement\Spider\Spider_Assembly_fineMesh_frictionDamp\urdf\Spider_Assembly_fineMesh_frictionDamp.urdf'
 
 client = bullet_client.BulletClient(connection_mode=p.GUI)
+
+
 client.setAdditionalSearchPath(pybullet_data.getDataPath())
 client.setGravity(0, 0, -9.81)
-
 plane = client.loadURDF('plane.urdf')
-client.changeDynamics(plane, -1, lateralFriction=0.8)
+client.changeDynamics(plane, -1, lateralFriction=0.9)
 
+camera_target_position = [0.0, 0.0, 0.0]
+camera_distance = 1.0
+camera_yaw = 64.0
+camera_pitch = -44.2
+client.resetDebugVisualizerCamera(camera_distance, camera_yaw, camera_pitch, camera_target_position)
+
+flags = client.URDF_USE_SELF_COLLISION
 hexapod = client.loadURDF(hexapod_urdf_path,
-                          [0, 0, 0.23])
+                          [0, 0, 0.23], flags=flags)
 hexapod_right_colors = parse_urdf_for_colors(hexapod_urdf_path)
 
+
+# client.changeDynamics(hexapod, -1, contactStiffness=1e4, contactDamping=1e3)
 for i, color in enumerate(hexapod_right_colors):
     client.changeVisualShape(hexapod, i-1, rgbaColor=color)
 
@@ -36,20 +46,23 @@ for joint_index in range(num_joints):
 
     joint_param_ids[joint_index] = client.addUserDebugParameter(joint_name, joint_range[0], joint_range[1], 0)
 
+p.setRealTimeSimulation(1)
 while 1:
 
+    a = client.getLinkState(hexapod, 0)
+    b = client.getEulerFromQuaternion(a[5]) # Roll, pitch, yaw of base
+    cal_angle = np.rad2deg(np.sqrt(b[0]**2 + b[1]**2))
+    client.addUserDebugText(f'base Orientation: {b[0]:.4f}, {b[1]:.4f}, {b[2]:.4f}, cal_angle: {cal_angle:.4f}',
+                            [0, 0, 0.3], lifeTime=0.5)
+
     for joint_index in range(num_joints):
-        joint_param_value = client.readUserDebugParameter(joint_param_ids[joint_index])
+        # joint_param_value = client.readUserDebugParameter(joint_param_ids[joint_index])
+        joint_param_value = np.random.rand(1) *2 -1
         client.setJointMotorControl2(hexapod, joint_index, client.POSITION_CONTROL, targetPosition=joint_param_value)
-    for _ in range(int(0.01 / (1/240))):
-        client.stepSimulation()
-        time.sleep(1 / 240)
 
 
 
-
-
-
+client.disconnect()
 
 
 
